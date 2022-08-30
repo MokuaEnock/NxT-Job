554405cb6c80a2e73b96529707b46a6dbaf71091
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-08-30 05:06:32 +0300

    hide section when catalogue is clicked

--- assets/script/dom.js
+++ assets/script/dom.js
@@ -1,14 +1,32 @@
-document.addEventListener("DOMContentLoaded", load);
+let login = document.querySelector("#login");
+let exploreJob = document.querySelector("#explore");
+let search = document.querySelector("#search");
+let results = document.querySelector("#results");
+let courses = document.querySelector("#courses");
+let header = document.querySelector("header");
+let main = document.querySelector("main");
+let nav = document.querySelector("nav");
+let services = document.querySelector('#services')
+let footer = document.querySelector('footer')
+
+/* ===============main page buttons===================== */
+let catalogueButton = document.querySelector("#catalogue");
 
 function load() {
-  let login = document.querySelector("#login");
-  let exploreJob = document.querySelector("#explore");
-  let search = document.querySelector("#search");
-  let results = document.querySelector("#results");
-  let courses = document.querySelector("#courses");
   login.style.display = "none";
   exploreJob.style.display = "none";
   search.style.display = "none";
   results.style.display = "none";
   courses.style.display = "none";
 }
+function toLogin() {
+  header.style.display = "none";
+  nav.style.display = "none";
+  main.style.display = "none";
+  services.style.display = "none";
+  footer.style.display = "none";
+  login.style.display = "block";
+}
+
+catalogueButton.addEventListener("click", toLogin);
+document.addEventListener("DOMContentLoaded", load);
