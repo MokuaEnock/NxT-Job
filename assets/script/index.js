8a561189f54ef9aa22dd7963cf700c2f4f65ea28
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-08-31 14:01:23 +0300

    create learn more section

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,8 +1,56 @@
-/* let url = "https://www.reed.co.uk/api/1.0/search?keywords=accountant&location=london&employerid=123&distancefromlocation=15";
-const apiKey = "abdb8ea5-2120-443f-8246-7442a083e2b2"
-fetch(url)
-  .then((data) => data.json())
-  .then((data) => {
-    console.log(data);
-  });
- */
+/*
+
+Adzuna jobs
+
+let url =
+  "https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=d0291057&app_key=e45310af6518f33ea0f2617638ff1d7f";
+
+head = {
+  method: "GET",
+  mode: "cors",
+  credentials: "same-origin",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+fetch(url, head)
+  .then((res) => res.json())
+  .then((data) => console.log(data.results))
+  .catch((error) => console.log("error", error)); */
+
+let udemy_client_id = "M8MBZMxs96KD52PJl6QRQ9JuiRMgGHL7zlQDiZtM";
+let udemy_client_secret =
+  "Cln0LIUPycJeUMBZ22kiEl1MTNlOBTEVUJtrbHlg9ocba1CNcrkp9tPRWFniYbCArBYTcnHpKXmxQRuTISaqLvyBhFxxM3B2EZje0vzDy0Tgh1VppI9GLwbAus1IEGIi";
+let url =
+  "https://www.udemy.com/api-2.0/courses/238934/?fields[course]=title,headline";
+
+/* head = {
+  method: "GET",
+  mode: "cors",
+  credentials: "same-origin",
+  headers: {
+    "Content-Type": "application/json",
+  },
+}; */
+
+let ter = btoa(udemy_client_id + ":" + udemy_client_secret);
+
+let headers = new Headers();
+headers.set(
+  "Authorization",
+  "Basic" + btoa(udemy_client_id + ":" + udemy_client_secret)
+);
+let cors =
+  "Access-Control-Allow-Origin: https://www.udemy.com/api-2.0/courses/238934/?fields[course]=title,headline";
+fetch(url, {
+  method: "GET",
+  mode: "cors",
+  credentials: "same-origin",
+  headers: {
+    Authorization: `Basic + ${ter}`,
+    "Access-Control-Allow-Origin": "http://127.0.0.1:5501",
+  },
+})
+  .then((res) => res.json())
+  .then((data) => console.log(data.results))
+  .catch((error) => console.log("error", error));
