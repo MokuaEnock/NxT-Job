3975ca1c23a19f6737a97234b6de49cfdbe13f6b
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-09-01 07:43:52 +0300

    get input value form job search value

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,7 +1,6 @@
 /* Adzuna jobs */
-
-let url =
-  "https://api.adzuna.com/v1/api/jobs/us/search/2?app_id=d0291057&app_key=e45310af6518f33ea0f2617638ff1d7f&results_per_page=100&what=software&sort_by=salary&salary_min=10000&content-type=application/json";
+/*
+let url =`https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=d0291057&app_key=e45310af6518f33ea0f2617638ff1d7f&results_per_page=1000&what=Machine%20Learning`;
 
 head = {
   method: "GET",
@@ -14,7 +13,25 @@ head = {
 fetch(url, head)
   .then((res) => res.json())
   .then((data) => {
-    let jobJson = JSON.stringify(data.results);
-    console.log(jobJson);
+    console.log(data.results);
   })
   .catch((error) => console.log("error", error));
+ */
+
+/* let search = document.querySelector("#search_info>span>input");
+
+function searchJob(e) {
+  e.preventDefault();
+  let searchTerm = search.value;
+  console.log(searchTerm);
+}
+ */
+
+let jobForm = document.querySelector("#search_info");
+
+jobForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let jobValue = document.querySelector("#job_search_value");
+  console.log(jobValue.value);
+  jobForm.reset();
+});
