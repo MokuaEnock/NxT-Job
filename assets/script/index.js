333d4828ddce8a30b24351b70a7db0ad8e1245fd
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-09-01 12:19:00 +0300

    create function to append card into dom

--- assets/script/index.js
+++ assets/script/index.js
@@ -46,6 +46,67 @@ jobForm.addEventListener("submit", (e) => {
   jobForm.reset();
 }); */
 
+function createCard(array) {
+  array.forEach((element) => {
+    let jobCard = document.createElement("div");
+    jobCard.className = "category_card";
+    // card header
+    let jobCardHeader = document.createElement("div");
+    jobCardHeader.className = "category_card_header_info";
+    let jobCardHeaderTitle = document.createElement("div");
+    jobCardHeaderTitle.className = "job_desc_title";
+    jobCardHeaderTitle.innerText = element.title;
+    let jobCardHeaderdesc = document.createElement("div");
+    jobCardHeaderdesc.className = "company_desc_name";
+    jobCardHeaderdesc.innerText = element.company.display_name;
+    jobCardHeader.appendChild(jobCardHeaderTitle);
+    jobCardHeader.append(jobCardHeaderdesc);
+    jobCard.append(jobCardHeader);
+    // card description
+    let jobCardBody = document.createElement("p");
+    jobCardBody.className = "body_job_desc";
+    jobCardBody.innerHTML = element.description;
+    jobCardBody.style.overflow = "hidden";
+    jobCard.append(jobCardBody);
+
+    // small body
+    let jobCardSmallBody = document.createElement("div");
+    jobCardSmallBody.className = "body_job_info";
+    let jobCardSpan1 = document.createElement("span");
+    let jobCardSpan2 = document.createElement("span");
+    let jobCardSpan3 = document.createElement("span");
+    jobCardSpan1.className = "body_job_inf_span";
+    jobCardSpan2.className = "body_job_inf_span";
+    jobCardSpan3.className = "body_job_inf_span";
+    jobCardSpan1.innerText = element.contract_time;
+    jobCardSpan2.innerText = element.location.area[1];
+    jobCardSpan3.innerText = element.category.__CLASS__.label;
+    jobCardSmallBody.appendChild(jobCardSpan1);
+    jobCardSmallBody.append(jobCardSpan2);
+    jobCardSmallBody.append(jobCardSpan3);
+    jobCard.append(jobCardSmallBody);
+
+    //card footer
+    let button1 = document.createElement("button");
+    button1.className = "view_more_job";
+    button1.innerHTML = "Learn More";
+    let button2 = document.createElement("button");
+    button2.className = "save_job";
+    let imageb = document.createElement("img");
+    imageb.src = "./assets/images/save-instagram-outline.png";
+    button2.appendChild(imageb);
+    let foot = document.createElement("div");
+    foot.className = "category_card_foot";
+    foot.appendChild(button1);
+    foot.append(button2);
+    jobCard.append(foot);
+    // append card
+    let results = document.querySelector("#results_2");
+    results.appendChild(jobCard);
+  });
+}
+
+/* =============================Working================================ */
 /* url = "http://localhost:3000/job";
 let jobForm = document.querySelector("#search_info");
 jobForm.addEventListener("submit", (e) => {
@@ -53,7 +114,6 @@ jobForm.addEventListener("submit", (e) => {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       let exploreJob = document.querySelector("#explore");
       exploreJob.style.display = "none";
       data.forEach((element) => {
@@ -114,5 +174,4 @@ jobForm.addEventListener("submit", (e) => {
         results.appendChild(jobCard);
       });
     });
-});
- */
+});*/
