4b4e57568c1c66cd5a5ad37e2a3776a4473d4125
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-09-20 22:56:12 +0300

    the muse working

--- pages/pages_styles/all.js
+++ pages/pages_styles/all.js
@@ -1,9 +1,7 @@
 /* ========================Arbeit Now api============Failed==================== */
-/* let requestOptions = {
+/* var requestOptions = {
   method: "GET",
   redirect: "follow",
-  mode: "no-cors",
-
 };
 
 fetch("https://arbeitnow.com/api/job-board-api", requestOptions)
@@ -71,35 +69,34 @@ fetch(url, {
   .then((res) => console.log(res))
   .catch((err) => console.log(err)); */
 
-/* The muse =======================failed ================================================== */
-
-/* let url = "https://www.themuse.com/api/public/jobs?page=1";
+/* The muse ========================================================================= */
 
 let apiKey = "7235e19056860ded17d2185272909e6481fb893c2ce1c89ffbfbe8379d65eb0b";
 
-fetch(url, { method: "GET", headers: {} });
+let url =
+  "https://www.themuse.com/api/public/jobs?items_per_page=100?category=Science%20and%20Engineering&page=1&descending=True?api_key=7235e19056860ded17d2185272909e6481fb893c2ce1c89ffbfbe8379d65eb0b?";
 
-fetch(url, {
-  method: "GET",
-  mode: "no-cors",
-  headers: {
-    "X-Auth-Token": apiKey,
-  },
-})
+fetch(url)
   .then((res) => res.json())
   .then((res) => console.log(res))
   .catch((err) => console.log(err));
- */
 
 /* ================================== Dev IT jobs =============================  */
 
-let url = "https://devitjobs.uk/job_feed.xml";
+/* let url = "https://devitjobs.uk/job_feed.xml";
 fetch(url).then((res) =>
   res
     .text()
     .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
-    .then((data) => console.log(data))
-);
+    .then((data) => {
+      console.log(data);
+    })
+); */
 
 /* ================================Adzunna Api=============================================== */
 
+/* let url1 = `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=d0291057&app_key=e45310af6518f33ea0f2617638ff1d7f&results_per_page=1000&what=fullstack%20developer}`;
+fetch(url1)
+  .then((res) => res.json())
+  .then((data) => console.log(data.results))
+  .catch((err) => console.log(err)); */
