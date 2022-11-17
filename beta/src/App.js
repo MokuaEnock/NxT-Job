ee9dd480e2aa765eed2bbc2dd5f2c1d81989ff3f
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-17 20:43:15 +0300

    connect elements

--- beta/src/App.js
+++ beta/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import Landing from "./components/landing";
+import Navbar from "./components/navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/login" />
+        <Route path="/" element={}/>
+        <Route path="/" element={}/>
+        <Route path="/" element={}/>
+        <Route path="/" element={}/>
+        <Route path="/" element={}/>
+        <Route path="/" element={}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ beta/src/components/landing.js
@@ -0,0 +1,3 @@
+export default function Landing() {
+  return <main>landing Page</main>;
+}
