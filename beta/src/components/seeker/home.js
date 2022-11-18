ac5bae0c21492d1437e9402f9a2e0feffd9b7bf9
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-18 10:55:43 +0300

    create skeleton of seeker page

--- beta/src/components/seeker/home.js
+++ beta/src/components/seeker/home.js
@@ -1,9 +1,19 @@
 export default function Seeker() {
+  function Form() {
+    return <form id="seeker-search"></form>;
+  }
 
+  function SeekerJobItem() {
+    return <li className="seeker-job-item"></li>;
+  }
 
   return (
     <section id="seeker-home-all">
-
+      <Form />
+      <section id="seeker-results">
+        <ul id="seeker-job-results"></ul>
+        <div id="seeker-job-view"></div>
+      </section>
     </section>
   );
 }
