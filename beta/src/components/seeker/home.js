305d9a8cd82b35a59918a42875b90eb0d79d6435
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-18 10:20:41 +0300

    restucture sekker view styles

--- beta/src/components/seeker/home.js
+++ beta/src/components/seeker/home.js
@@ -1,37 +1,9 @@
 export default function Seeker() {
-  function Form() {
-    return (
-      <section id="seeker-home">
-        <form id="seeker-home-search">
-          <span>Search for your next Job</span>
-          <span>
-            <input type="text" placeholder="What" />
-            <input type="text" placeholder="Where" />
-            <button type="submit">Search</button>
-          </span>
-        </form>
-      </section>
-    );
-  }
 
-  function Joblist() {
-    return <></>;
-  }
-
-  function JobItem() {
-    return <></>;
-  }
-
-  function JobView() {
-    return <></>;
-  }
 
   return (
-    <>
-      <Form />
-      <Joblist />
-      <JobItem />
-      <JobView />
-    </>
+    <section id="seeker-home-all">
+
+    </section>
   );
 }
